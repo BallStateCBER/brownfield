61e6fa81b5177c8e6d5f2df0efc3870235184042
Graham Watson <graham@phantomwatson.com>
2013-11-14 14:34:57 -0500

    Add topic()

--- Controller/PagesController.php
+++ Controller/PagesController.php
@@ -62,4 +62,96 @@ class PagesController extends AppController {
 	public function select_chart($state, $county) {
 		$this->redirect("/$state/$county/population");
 	}
+	
+	/* Accepts $state and $county as either strings (simplified names) or integers (ID #s)
+	 * If no topic is provided, will default to the first topic listed in ReportsController::getTopicList() */
+	public function topic($state = null) {
+		// Collect parameters from the URL
+		$path_parts = explode('/', $this->request->params['url']['url']);
+		$county = isset($path_parts[1]) ? $path_parts[1] : null;
+		$topic = (isset($path_parts[2]) && ! empty($path_parts[2])) ? $path_parts[2] : null;
+
+		// Validate county
+		$state_id = is_numeric($state) ? $state : $this->Location->getStateID($state);
+		$state_full_name = $this->Location->getStateFullName($state_id);
+		if (! $this->Location->isCountyInState($county, $state_id)) {
+			$this->Flash->error("Error: County '$county[1]' not found in $state_full_name.");
+			$this->redirect('/');
+		}
+
+		// Redirect to the first topic if none is specified
+		$this->loadModel('Report');
+		if (! $topic) {
+			$this->redirect('/'.$path_parts[0].'/'.$path_parts[1].'/'.$this->Report->getFirstTopic());
+		}
+
+		// Validate topic
+		if ($topic != 'all_charts' && ! $this->Report->isTopic($topic)) {
+			$this->set(array(
+				'invalidTopicSelected' => $topic
+			));
+			$this->render('/errors/invalid_topic');
+			return false;
+		}
+
+		// Get human-readable state, county, and topic names to put into this page's title
+		$state_abbreviation = $this->Location->getStateAbbreviation($state_id);
+		$county_id = is_numeric($county) ? $county : $this->Location->getCountyID($county, $state_id);
+		$county_full_name = $this->Location->getCountyFullName($county_id, $state_id);
+
+		// Set variables used in either topic page or all-charts page
+		$this->set(array(
+			'county_id' => $county_id,
+			'state_id' => $state_id,
+			'state_abbreviation' => strtolower($state_abbreviation),
+			'county_name_simplified' => $this->Location->simplify($county_full_name)
+		));
+
+		// 'All charts' page
+		if ($topic == 'all_charts') {
+			$this->set(array(
+				'title_for_layout' => "$county_full_name, $state_abbreviation - All Charts",
+				'topics' => $this->Report->getTopicList(true) // Categorized topic list
+			));
+			return $this->render('all_charts');
+		}
+
+		// Set variables and render topic page
+		$this->loadModel('ChartDescription');
+		$topic_full_name = $this->Report->getTopicFullName($topic);
+		$this->set(array(
+			'selected_topic' => $topic,
+			'description' => $this->ChartDescription->getDescription($topic),
+			'title_for_layout' => "$county_full_name, $state_abbreviation $topic_full_name",
+			'topic_full_name' => $topic_full_name,
+			'chart_availability' => $this->requestAction(
+				array('controller' => 'reports', 'action' => 'getStatus'),
+				array('pass' => array('chart', $topic, $state_id, $county_id))
+			),
+			'table_availability' => $this->requestAction(
+				array('controller' => 'reports', 'action' => 'getStatus'),
+				array('pass' => array('table', $topic, $state_id, $county_id))
+			),
+			'csv_availability' => $this->requestAction(
+				array('controller' => 'reports', 'action' => 'getStatus'),
+				array('pass' => array('csv', $topic, $state_id, $county_id))
+			),
+			'excel5_availability' => $this->requestAction(
+				array('controller' => 'reports', 'action' => 'getStatus'),
+				array('pass' => array('excel5', $topic, $state_id, $county_id))
+			),
+			'excel2007_availability' => $this->requestAction(
+				array('controller' => 'reports', 'action' => 'getStatus'),
+				array('pass' => array('excel2007', $topic, $state_id, $county_id))
+			),
+			'source_availability' => $this->requestAction(
+				array('controller' => 'reports', 'action' => 'getStatus'),
+				array('pass' => array('source', $topic, $state_id, $county_id))
+			),
+			'sources' => $this->requestAction(
+				array('controller' => 'reports', 'action' => 'switchboard'),
+				array('pass' => array('source', $topic, $state_id, $county_id))
+			)
+		));
+	}
 }
