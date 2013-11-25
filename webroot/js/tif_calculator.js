b4164760d33b33add472648d0fed14fcb039a16e
Graham Watson <graham@phantomwatson.com>
2013-11-25 15:32:43 -0500

    Convert Prototype.js to jQuery
    Also remove intro text toggling functions

--- webroot/js/tif_calculator.js
+++ webroot/js/tif_calculator.js
@@ -27,10 +27,10 @@ function initializeTIFCalculator() {
 		$(this).val(addCommas($(this).val()));
 	});
 	
-	var county_id = $('calc_county_id').getValue();
-	var industry_id_select = $('calc_industry_id');
+	var county_id = $('#calc_county_id').val();
+	var industry_id_select = $('#calc_industry_id');
 	var industry_index = industry_id_select.selectedIndex;
-	var input_options_select = $('calc_input_options');
+	var input_options_select = $('#calc_input_options');
 	var input_option_index = input_options_select.selectedIndex;
 	
 	// Handles situations where the user reaching this page by refreshing or going back in their
@@ -42,7 +42,7 @@ function initializeTIFCalculator() {
 			if (input_option_index != 0) {
 				input_options_select.selectedIndex = input_option_index;
 				onInputMethodSelection(input_option_index);
-				(input_option_index == 1) ? user_input = $('calc_annual_production').getValue() : user_input = $('calc_employees').getValue();
+				(input_option_index == 1) ? user_input = $('#calc_annual_production').val() : user_input = $('#calc_employees').val();
 				if (inputToInt(user_input)) {
 					calculateImpact(false);
 				}
@@ -56,33 +56,33 @@ function initializeTIFCalculator() {
  * (or shows all industries if there's an error looking the industries up)
  * and resets both industry selection and input-type selection if reset_subsequent is set to TRUE. */
 function onCountySelection(county_id, reset_subsequent) {
-	$('calc_county_id_leading_choice').hide();
-	var industry_select = $('calc_industry_id');
-	industry_select.enable();
+	$('#calc_county_id_leading_choice').hide();
+	var industry_select = $('#calc_industry_id');
+	industry_select.removeAttr('disabled');
 	if (reset_subsequent) {
 		resetInputOptions();
-		$('calc_input_options').disable();
-		$('calculate_button').disable();
+		$('#calc_input_options').attr('disabled', 'disabled');
+		$('#calculate_button').attr('disabled', 'disabled');
 	}
 	var url = '/calculators/getLocalIndustries/' + county_id;
-	new Ajax.Request(url, {
-		method: 'get',
-		onSuccess: function(transport) {
-			if (transport.responseText.match('Error')) {
-				alert('Error finding industries for this county: ' + transport.responseText);
-				industry_select.childElements().each(function(option) {
-					option.show();
+	$.ajax({
+		url: url
+		success: function(data) {
+			if (data.match('Error')) {
+				alert('Error finding industries for this county: ' + data);
+				industry_select.children().each(function () {
+					$(this).show();
 				});
 			} else {
-				var industry_ids = $w(transport.responseText);
+				var industry_ids = data.split(' ');
 				var local_industry_count = industry_ids.length;
 				if (reset_subsequent) {
 					industry_select.selectedIndex = 0;
 				}
 				if (local_industry_count > 0) {
-					var options = $$('option.foo_option');
-					options.each(function(option) {
-						var industry_id = option.value;
+					var options = $('option.foo_option');
+					options.each(function (option) {
+						var industry_id = option.val();
 						pos = industry_ids.indexOf(industry_id);
 						if (pos == -1) {
 							option.hide();
@@ -94,8 +94,8 @@ function onCountySelection(county_id, reset_subsequent) {
 
 			}
 		},
-		onFailure: function() {
-			var options = $$('option.foo_option');
+		error: function() {
+			var options = $('option.foo_option');
 			options.each(function(option) {
 				option.show();
 			});
@@ -104,10 +104,10 @@ function onCountySelection(county_id, reset_subsequent) {
 }
 
 function onIndustrySelection(reset_subsequent) {
-	$('calc_industry_id_leading_choice').hide();
-	$('calculate_button').disable();
-	var calc_input_options = $('calc_input_options');
-	calc_input_options.enable();
+	$('#calc_industry_id_leading_choice').hide();
+	$('#calculate_button').attr('disabled', 'disabled');
+	var calc_input_options = $('#calc_input_options');
+	calc_input_options.removeAttr('disabled');
 	if (calc_input_options.selectedIndex == 0) {
 		resetInputOptions();
 	} else {
@@ -116,59 +116,52 @@ function onIndustrySelection(reset_subsequent) {
 }
 
 function resetInputOptions() {
-	$('option_a_input').hide();
-	$('option_b_input').hide();
-	$('calc_input_option_leading_choice').show();
-	$('calc_input_options').selectedIndex = 0;
+	$('#option_a_input').hide();
+	$('#option_b_input').hide();
+	$('#calc_input_option_leading_choice').show();
+	$('#calc_input_options').selectedIndex = 0;
 }
 
 function onInputMethodSelection(selected_index) {
-	$('calc_input_option_leading_choice').hide();
+	$('#calc_input_option_leading_choice').hide();
 	if (selected_index == 1) { // option A
-		$('option_a_input').show();
-		$('option_b_input').hide();
+		$('#option_a_input').show();
+		$('#option_b_input').hide();
 	} else if (selected_index == 2) { // option B
-		$('option_a_input').hide();
-		$('option_b_input').show();
+		$('#option_a_input').hide();
+		$('#option_b_input').show();
 	}
-	$('calculate_button').enable();
+	$('#calculate_button').removeAttr('disabled');
 }
 
 function calculateImpact(animate) {
-	var county_id = $('calc_county_id').getValue();
-	var industry_id = $('calc_industry_id').getValue();
-	var selected_option = $('calc_input_options').selectedIndex;
+	var county_id = $('#calc_county_id').val();
+	var industry_id = $('#calc_industry_id').val();
+	var selected_option = $('#calc_input_options').selectedIndex;
 	if (! county_id || ! industry_id) {
 		return;
 	}
 	if (selected_option == 1) {
-		var annual_production = inputToInt($('calc_annual_production').getValue());
+		var annual_production = inputToInt($('#calc_annual_production').val());
 		if (! annual_production) {
 			return alert('Please enter the expected annual production of this company (in dollars).');
 		}
 		var url = '/calculators/tif_output/county_id:' + county_id + '/industry_id:' + industry_id + '/annual_production:' + annual_production + '/option:a';
 	} else {
-		var employees = inputToInt($('calc_employees').getValue());
+		var employees = inputToInt($('#calc_employees').val());
 		if (! employees) {
 			return alert('Please enter the expected number of employees for this company.');
 		}
 		var url = '/calculators/tif_output/county_id:' + county_id + '/industry_id:' + industry_id + '/employees:' + employees + '/option:b';
 	}
-	var container = $('calc_output_container');
-	var calc_loading_graphic_container = $('calc_loading_graphic_container');
+	var container = $('#calc_output_container');
+	var calc_loading_graphic_container = $('#calc_loading_graphic_container');
 	hideCalcIntroText(animate);
-	if (animate && container.visible()) {
-		$$('.calc_section').each(function(section){
-			Effect.DropOut(section, {
-				duration: 0.2,
-				queue: {position: 'front', scope: 'calc_section_dropout'},
-				afterFinish: function() {
-					var queue = Effect.Queues.get('calc_section_dropout');
-					if (queue.size() == 0) {
-						container.hide();
-						updateCalculatorOutput(url, animate);
-					}
-				}
+	if (animate && container.is(':visible')) {
+		$('.calc_section').each(function () {
+			$(this).slideUp(200, function () {
+				container.hide();
+				updateCalculatorOutput(url, animate);
 			});
 		});
 		/*
@@ -185,31 +178,30 @@ function calculateImpact(animate) {
 }
 
 function updateCalculatorOutput(url, animate) {
-	var container = $('calc_output_container');
-	var calc_loading_graphic_container = $('calc_loading_graphic_container');
+	var container = $('#calc_output_container');
+	var calc_loading_graphic_container = $('#calc_loading_graphic_container');
 	if (animate) {
 		var slide_duration = 0.8;
 		var loading_fade_duration = 0.5;
-		calc_loading_graphic_container.appear({
-			duration: loading_fade_duration,
-			afterFinish: function() {
-				var myAjax = new Ajax.Updater(container, url, {
-					method: 'get',
-					onComplete: function() {
-						calc_loading_graphic_container.fade({duration: loading_fade_duration});
-						Effect.SlideDown(container, {
-							queue: {position: 'end', scope: 'calculator', limit: 1},
-							duration: slide_duration
-						});
+		calc_loading_graphic_container.fadeIn(
+			loading_fade_duration,
+			function () {
+				$.ajax({
+					url: url,
+					complete: function (data) {
+						container.html(data);
+						calc_loading_graphic_container.fadeIn(loading_fade_duration);
+						container.slideDown(slide_duration);
 					}
 				});
 			}
-		});
+		);
 	} else {
 		calc_loading_graphic_container.show();
-		var myAjax = new Ajax.Updater(container, url, {
-			method: 'get',
-			onComplete: function() {
+		$.ajax({
+			url: url,
+			complete: function (data) {
+				container.html(data);
 				calc_loading_graphic_container.hide();
 				container.show();
 			}
@@ -237,38 +229,4 @@ function inputToInt(input) {
 		input = input.substring(0, index_of_point);
 	}
 	return input.replace(/[^0-9]/g, '');
-}
-
-function showCalcIntroText() {
-	var intro_text_teaser = $('calc_intro_text_teaser');
-	if (! intro_text_teaser.visible()) {
-		return;
-	}
-	intro_text_teaser.hide();
-	Effect.SlideDown('calc_intro_text', {
-		queue: {position: 'end', scope: 'calculator_intro_text', limit: 1},
-		duration: 0.5
-	});
-}
-function hideCalcIntroText(animate) {
-	var intro_text = $('calc_intro_text');
-	if (! intro_text) {
-		return;
-	}
-	var intro_text_teaser = $('calc_intro_text_teaser');
-	if (! intro_text.visible()) {
-		return;
-	}
-	if (animate) {
-		Effect.SlideUp(intro_text, {
-			queue: {position: 'end', scope: 'calculator_intro_text', limit: 1},
-			duration: 0.5,
-			afterFinish: function() {
-				intro_text_teaser.show();
-			}
-		});
-	} else {
-		intro_text.hide();
-		intro_text_teaser.show();
-	}
 }
