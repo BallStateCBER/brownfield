0922ce9c7260f0d4bf282fc302bf493442d7b2a1
Graham Watson <graham@phantomwatson.com>
2013-12-30 18:17:29 -0500

    Rename to SvgChartReport and extend Report model

---
+++ Model/SvgChartReport.php
@@ -0,0 +1,225 @@
+<?php
+App::uses('GoogleCharts', 'GoogleCharts.Lib');
+App::uses('Datum', 'Model');
+App::uses('Location', 'Model');
+App::Import ('model', 'Report');
+
+class SvgChartReport extends Report {
+	public $useTable = false;
+	
+	public $category_id = null;
+	public $locations = array();	// Each member is an array of [loc_type_id, loc_id]
+	
+	public $defaultOptions = array(
+		'width' => 510,
+		'height' => 300,
+		'legend' => array(
+			'position' => 'bottom',
+			'alignment' => 'center'
+		),
+		'titleTextStyle' => array(
+			'color' => 'black',
+			'fontSize' => 16
+		),
+		'vAxis' => array(
+			'textStyle' => array(
+				'fontSize' => 12
+			)
+		)
+	);
+	
+	// Supplied by getTable()'s parameters
+	public $segment = null;
+	public $data = array();
+	public $segmentParams = array();
+	public $structure = array();
+	
+	// Set by segment-specific methods
+	public $type = null;		// e.g. BarChart
+	public $rows = array();		// array(array('category' => 'Foo', 'value' => 123), ...)
+	public $columns = array();	
+	public $options = array();	// Includes 'title', etc.
+	public $footnote = "";
+	public $callbacks = array();// array('eventName' => 'functionName or anonymous function')
+	
+	/* Chart::getChartList() should be updated whenever charts are added or when their method 
+	 * names or human-readable titles are changed. This is used by the navigation sidebar and 
+	 * to test whether or not user-supplied chart names are valid. */
+	public function getChartList() {
+		return array(
+			'demographics' => array(
+				'population' => 'Population',
+				'population_growth' => 'Population Growth',
+				'density' => 'Population and Housing Units Density',
+				'population_age_breakdown' => 'Population by Age',
+				'female_age_breakdown' => 'Female Age Breakdown',
+				'population_by_sex' => 'Population By Sex',
+				'dependency_ratios' => 'Dependency Ratios',
+				'educational_attainment' => 'Educational Attainment',
+				'graduation_rate' => 'High School Graduation Rates',
+				'household_size' => 'Average Household Size',
+				'households_with_minors' => 'Households With People Under 18',
+				'household_types_with_minors' => 'Households With People Under 18, Breakdown By Type',
+				'households_with_over_65' => 'Households With People Over 65',
+				'poverty' => 'Poverty',
+				'lunches' => 'Free and Reduced Lunches',
+				'disabled' => 'Disabled Population',
+				'disabled_ages' => 'Disabled Age Breakdown',
+				'public_assistance' => 'Public Assistance'
+			),
+			'economy' => array(
+				'share_of_establishments' => 'Percent Share of Total Establishments',
+				'employment_growth' => 'Employment Growth',
+				'employment_trend' => 'Employment Trend',
+				'unemployment_rate' => 'Unemployment Rate',
+				'personal_and_household_income' => 'Personal and Household Income',
+				'income_inequality' => 'Income Inequality',
+				'federal_spending' => 'Federal Spending'
+			),
+			'health' => array(
+				'birth_rate' => 'Crude Birth Rate',
+				'birth_rate_by_age' => 'Birth Rate by Age Group',
+				'birth_measures' => 'Birth Measures',
+				'fertility_rates' => 'Fertility Rates',
+				'deaths_by_sex' => 'Deaths By Sex',
+				'death_rate' => 'Death Rate',
+				'death_rate_by_cause' => 'Death Rate By Cause',
+				'infant_mortality' => 'Infant Mortality',
+				'life_expectancy' => 'Life Expectancy',
+				'years_of_potential_life_lost' => 'Years of Potential Life Lost',
+				'self_rated_poor_health' => 'Self-Rated Poor Health',
+				'unhealthy_days' => 'Average Unhealthy Days per Month',
+				'cancer_death_and_incidence_rates' => 'Cancer Death and Incidence Rates',
+				'lung_diseases' => 'Lung Diseases'
+			)
+		);
+	}
+	
+	public function isValidChart($action) {
+		$all_charts = $this->getChartList();
+		$is_chart = false;
+		foreach ($all_charts as $tab => $tabs_charts) {
+			foreach ($tabs_charts as $chart_code => $chart_title) {
+				if ($chart_code == $action) {
+					$is_chart = true;
+					break 2;
+				}
+			}
+		}
+		return $is_chart;
+	}
+	
+	public function isValidCounty($county_id) {
+		return (is_numeric($county_id) && $county_id >= 1 && $county_id <= 92);
+	}
+	
+	private function getStartYear() {
+		$dates = array_keys($this->values);
+		return substr(reset($dates), 0, 4);
+	}
+	
+	private function getEndYear() {
+		$dates = array_keys($this->values);
+		return substr(end($dates), 0, 4);
+	}
+	
+	// If no parameters are passed, assumes the first location specified in $this->locations
+	private function getLocName($loc_type_id = null, $loc_id = null, $full_name = false) {
+		if ($loc_type_id == null) {
+			$loc_type_id = $this->locations[0][0];
+			$loc_id = $this->locations[0][1];	
+		}
+		$Location = new Location();
+		return $Location->getLocationName($loc_type_id, $loc_id, $full_name);
+	}
+	
+	/**
+	 * Returns the appropriate label for the year(s) included in this data set
+	 */
+	public function getYears() {
+		// If 'dates' were not specified (so all available dates are collected),
+		// populate $this->segmentParams['dates'] from the collected data
+		if (empty($this->segmentParams['dates'])) {
+			foreach ($this->data as $category_id => $loc_keys) {
+				foreach ($loc_keys as $loc_key => $dates) { 
+					foreach ($dates as $date => $value) {
+						if (! in_array($date, $this->segmentParams['dates'])) {
+							$this->segmentParams['dates'][] = $date;
+						}
+					}
+				}
+			}
+		}
+		$max_year = substr(max($this->segmentParams['dates']), 0, 4);
+		$min_year = substr(min($this->segmentParams['dates']), 0, 4);
+		if ($max_year == $min_year) {
+			return $max_year;
+		} else {
+			return "$min_year-$max_year";	
+		}
+	}
+	
+	/**
+	 * @param string $segment Equal to the name of a chart-generating method in this class
+	 * @param array $data
+	 * @param array $segment_params Keys may include 'locations', 'categories', 'dates', and 'county_id'
+	 * @param array $structure Optional array passed to Chart to help with rearranging data
+	 * @return GoogleCharts
+	 */
+	public function getChart($segment, $data, $segment_params, $structure) {
+		// TODO: Change references to 'segment' (used in County Profiles) into 'topic' (used in this site, more intuitive)
+		$this->segment = $segment;
+		$this->data = $data;
+		$this->segmentParams = $segment_params;
+		$this->structure = $structure;
+		
+		if (! method_exists($this, $segment)) {
+			return array();
+		}
+		
+		$this->{$segment}();
+		
+		$this->options = array_merge($this->defaultOptions, $this->options);
+		
+		$chart = new GoogleCharts(null, null, null, null, 'chart_'.$this->segment);
+		$chart->type($this->type)
+		    ->options($this->options) 
+		    ->columns($this->columns)
+		    ->callbacks($this->callbacks);
+		foreach ($this->rows as $row) {
+			$chart->addRow($row);
+		}
+		return array(
+			'chart' => $chart,
+			'footnote' => $this->footnote
+		);
+	}
+
+	public function population() {
+		$this->type = 'LineChart';
+		$county_id = $this->segmentParams['locations'][0]['id'];
+		$county_name = $this->getCountyName();
+		$year = $this->getYears();
+		$title = "Population of $county_name County, Indiana (".$year.')';
+		$this->options = array(
+			'title' => "$county_name\n$title\n($year)",
+			'legend' => array('position' => 'none'),
+		);
+		$this->columns = array(
+	        'category' => array('label' => 'Category', 'type' => 'string'),
+	        'value' => array('label' => 'Population', 'type' => 'number')
+	    );
+	    
+		foreach ($this->data as $category_id => $loc_keys) {
+			foreach ($loc_keys as $loc_key => $dates) { 
+				foreach ($dates as $date => $value) {
+					$year = substr($date, 0, 4);
+					$this->rows[] = array(
+						'category' => $year, 
+						'value' => $value
+					);
+				}
+			}
+		}	
+	}
+}
