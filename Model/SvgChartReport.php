3c761d9b62f6a9ee89bde04060eade903e8fdb8d
Graham Watson <graham@phantomwatson.com>
2014-01-16 13:46:10 -0500

    Add dependency_ratios()

--- Model/SvgChartReport.php
+++ Model/SvgChartReport.php
@@ -575,4 +575,58 @@ class SvgChartReport extends Report {
 		));
 		$this->prepDataAxis('percent', 0, 'h');
 	}
+
+	public function dependency_ratios() {
+		// Create chart
+		$this->chart = new GoogleCharts();
+		$this->applyDefaultOptions();
+		$this->chart->type("ColumnChart");
+		$county_name = $this->locations[0][2];
+		$this->chart->columns(array(
+	        'category' => array(
+	        	'label' => 'Sex', 
+	        	'type' => 'string'
+			),
+	        'county_value' => array(
+	        	'label' => $county_name, 
+	        	'type' => 'number'
+			),
+			'state_value' => array(
+	        	'label' => 'Indiana', 
+	        	'type' => 'number'
+			),
+			'country_value' => array(
+	        	'label' => 'United States', 
+	        	'type' => 'number'
+			),
+	    ));
+		
+		// Gather data
+		foreach ($this->data_categories as $label => $category_id) {
+			foreach ($this->locations as $loc_key => $location) {
+				$this->values[$loc_key][$label] = $this->Datum->getValue($category_id, $location[0], $location[1], $this->year);
+			}
+		}
+		
+		// Add bars
+		$categories = array_keys($this->data_categories);
+		foreach ($categories as $category) {
+			$values = array();
+			foreach ($this->locations as $key => $set) {
+				$values[] = $this->values[$key][$category];
+			}
+			$this->chart->addRow(array(
+				'category' => $category, 
+				'county_value' => $values[0],
+				'state_value' => $values[1],
+				'country_value' => $values[2]
+			));
+		}
+		
+		// Finalize
+		$year = $this->getYears();
+		$this->applyOptions(array(
+			'title' => 'Dependency Ratio Per 100 People ('.$year.')'
+		));
+	}
 }
