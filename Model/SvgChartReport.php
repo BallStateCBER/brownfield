aacb154ab90fd5d7dd8ed0f50f73cf73ba1427a5
Graham Watson <graham@phantomwatson.com>
2014-01-21 15:52:37 -0500

    Add colors to female_age_breakdown()

--- Model/SvgChartReport.php
+++ Model/SvgChartReport.php
@@ -547,6 +547,7 @@ class SvgChartReport extends Report {
 		// Finalize
 		$year = $this->getYears();
 		$this->applyOptions(array(
+			'colors' => array_slice($this->colors, 0, 3),
 			'isStacked' => true,
 			'title' => 'Female Age Breakdown ('.$year.')'
 		));
