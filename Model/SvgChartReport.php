98b26f8342abe48cdf4c50d331ef5da17ca1ec92
Graham Watson <graham@phantomwatson.com>
2014-01-24 16:20:39 -0500

    Add birth_rate()

--- Model/SvgChartReport.php
+++ Model/SvgChartReport.php
@@ -1824,4 +1824,69 @@ class SvgChartReport extends Report {
 			)
 		));
 	}
+
+	public function birth_rate() {
+		// Create chart
+		$this->chart = new GoogleCharts();
+		$this->applyDefaultOptions();
+		$this->chart->type("BarChart");
+		$this->chart->columns(array(
+	        'category' => array(
+	        	'label' => 'Location',
+	        	'type' => 'string'
+			),
+	        'value' => array(
+	        	'label' => 'Birth Rate',
+	        	'type' => 'number'
+			),
+			'annotation' => array(
+				'label' => 'Annotation',
+				'type' => 'string',
+				'role' => 'annotation'
+			),
+			'colors' => array(
+				'label' => 'Colors',
+				'type' => 'string',
+				'role' => 'style'
+			)
+	    ));
+		
+		// Gather data
+		foreach ($this->data_categories as $label => $category_id) {
+			foreach ($this->locations as $loc_key => $location) {
+				$this->values[$loc_key][$label] = $this->Datum->getValue($category_id, $location[0], $location[1], $this->year);
+			}
+		}
+		
+		// Add bars
+		$i = 0;
+		foreach ($this->locations as $loc_key => $location) {
+			$row = array(
+				'category' => $location[2]
+			);
+			foreach ($this->data_categories as $label => $category_id) {
+				$value = $this->values[$loc_key][$label];
+				$row['value'] = $value;
+				$row['annotation'] = $value;
+			}
+			$row['colors'] = $this->colors[$i];
+			$this->chart->addRow($row);
+			$i++;
+		}
+		
+		// Finalize
+		$year = $this->getYears();
+		$this->applyOptions(array(
+			'chartArea' => array(
+				'left' => 150
+			),
+			'legend' => array(
+				'position' => 'none'
+			),
+			'title' => 'Crude Birth Rate ('.$year.")\nLive Births per 1,000 Population",
+			'hAxis' => array(
+				'minValue' => 0
+			)
+		));
+	}
 }
