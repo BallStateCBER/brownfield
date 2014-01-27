4e23657674c4b92c6215a231a0b358b1e8584315
Graham Watson <graham@phantomwatson.com>
2014-01-27 16:26:00 -0500

    Add death_rate_by_cause()

--- Model/SvgChartReport.php
+++ Model/SvgChartReport.php
@@ -2210,4 +2210,76 @@ class SvgChartReport extends Report {
 			'title' => "Age-Adjusted Death Rate, All Causes ($year)"
 		));
 	}
+
+	public function death_rate_by_cause() {
+		// Create chart
+		$this->chart = new GoogleCharts();
+		$this->applyDefaultOptions();
+		$this->chart->type("BarChart");
+		$county_name = $this->locations[0][2];
+		$this->chart->columns(array(
+	        'category' => array(
+	        	'label' => 'Category', 
+	        	'type' => 'string'
+			),
+	        'county_value' => array(
+	        	'label' => $county_name, 
+	        	'type' => 'number',
+	        	'format' => '#,###'
+			),
+			'county_annotation' => array(
+				'label' => 'Annotation',
+				'type' => 'string',
+				'role' => 'annotation'
+			),
+			'state_value' => array(
+	        	'label' => 'Indiana',
+	        	'type' => 'number',
+	        	'format' => '#,###'
+			),
+			'state_annotation' => array(
+				'label' => 'Annotation',
+				'type' => 'string',
+				'role' => 'annotation'
+			)
+	    ));
+		
+		// Gather data
+		foreach ($this->data_categories as $label => $category_id) {
+			foreach ($this->locations as $loc_key => $location) {
+				$this->values[$loc_key][$label] = $this->Datum->getValue($category_id, $location[0], $location[1], $this->year);
+			}
+		}
+		
+		// Add bars
+		foreach ($this->data_categories as $label => $category_id) {
+			$values = array();
+			foreach ($this->locations as $key => $set) {
+				$values[] = $this->values[$key][$label];
+			}
+			$this->chart->addRow(array(
+				'category' => str_replace('\'', '\\\'', $label), 
+				'county_value' => $values[0],
+				'county_annotation' => number_format($values[0]),
+				'state_value' => $values[1],
+				'state_annotation' => number_format($values[1])
+			));
+		}
+		
+		// Finalize
+		$years = $this->getYears();
+		$this->applyOptions(array(
+			'chartArea' => array(
+				'left' => 270
+			),
+			'colors' => array_slice($this->colors, 0, 2),
+			'height' => 700,
+			'title' => "Age-Adjusted Death Rate by Cause ($years)",
+			'vAxis' => array(
+				'textStyle' => array(
+					'fontSize' => 12
+				)
+			)
+		));
+	}
 }
